190221547c1117a27eb3b79ef449af8275053905
scentmay <scentmay@gmail.com>
2022-06-15 10:40:06 +0000

    Subido ejercicio. Duda de por qué no se ejecuta función popBalloon

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const container = document.querySelector(".container");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let balloons = [
+  "pink",
+  "black",
+  "red",
+  "blue",
+  "yellow",
+  "green",
+  "null",
+  "black",
+  "red",
+  "null",
+  "yellow",
+  "green"
+];
+
+function popBalloon(pos) {
+  balloons[pos] = null;
+  renderBalloon();
+}
+
+function renderBalloon() {
+  let content = "";
+  //Por cada color en el array balloons, generamos un globo a través de JS  y lo mostramos en la página con innerhtml:
+  balloons.forEach(function(color, position) {
+    var visible = "visibled";
+    if (color == "null") {
+      visible = "popped";
+    }
+
+    //Generamos los globos
+    content += `<div class="balloon ${visible}" 
+      style="background: ${color}"
+      onclick="popBalloon(${position})"
+      ></div>`;
+    console.log(visible);
+  });
+  container.innerHTML = content;
+}
+
+window.onload = renderBalloon();
